e311d865f389cfe0b117677414a94524d76140b9
Bilgekaan Yilmaz <bilgekaanyilmaz@mbpvonbilgekaan.speedport_w_724v_09011603_06_010>
2022-07-22 22:51:47 +0200

    feature# implement addition and remove functions for the basket

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,17 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    console.log("item: ", item);
+    cartCtx.addItem({
+      ...item,
+      amount: 1,
+    });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
